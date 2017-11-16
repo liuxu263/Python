f1a3c0b02d438d96bca0525e0f6e5948e395c32b
liuxu <1308460167@qq.com>
2017-11-16 16:20:08 +0800

    新增菜鸟教程，实例10到实例15

---
+++ Python菜鸟教程实战100例/实例15.py
@@ -0,0 +1,15 @@
+#！ /usr/bin/env python 3
+# -*- coding: utf-8 -*-
+
+'学习成绩>=90分的同学用A表示，60-89分之间的用B表示，60分以下的用C表示。'
+
+__author__='lx'
+
+score=int(input('输入分数：'))
+if score >= 90:
+    grade = 'A'
+elif score >=60:
+    grade = 'B'
+else:
+    grade = 'C'
+print("%d属于%s"%(score,grade))
