239938e5ee481ba7ff5628c7afb0258b6527f984
liuxu <1308460167@qq.com>
2017-11-18 15:35:37 +0800

    增加菜鸟教程实例16到20

---
+++ Python菜鸟教程实战100例/实例16.py
@@ -0,0 +1,25 @@
+#！ /usr/bin/env python 3
+# -*— coding：utf-8 -*—
+
+'输出指定格式的日期'
+
+__author__='lx'
+
+import datetime
+
+if __name__ == '__main__':
+
+    #输出今日日期，格式为dd/mm/yyyy
+    print(datetime.date.today().strftime('%d/%m/%Y'))
+
+    #创建日期对象
+    miyazakiBirthDate =datetime.date(1941,1,5)
+    print(miyazakiBirthDate.strftime('%d/%m/%Y'))
+
+    #日期算术运算
+    miyazakiBirthNextDay =miyazakiBirthDate+datetime.timedelta(days=1)
+    print(miyazakiBirthNextDay.strftime('%d/%m/%Y'))
+
+    #日期替换
+    miyazakiFirstBirthday=miyazakiBirthDate.replace(year=miyazakiBirthDate.year+1)
+    print(miyazakiBirthDate.strftime('%d/%m/%Y'))
