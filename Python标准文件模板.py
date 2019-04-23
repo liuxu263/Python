b49dffe5c225fd7a1531d00556c60524c27e00c3
liuxu <liuxu@xiaozhu.com>
2019-04-23 17:41:19 +0800

    up

--- Python标准文件模板.py
+++ Python标准文件模板.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3 标准注释 可以让.py文件直接在Unix/Linux/Mac上运行
+#! /usr/local/bin/python3.7 标准注释 可以让.py文件直接在Unix/Linux/Mac上运行
 # -*- coding: utf-8 -*- 标准注释 表示.py文件本身使用标准UTF-8编码
 
 '模块的文档注释'
--- lx/testPython.py
+++
@@ -1,8 +0,0 @@
-#! /usr/local/bin/python3
-# -*- coding：utf-8 -*-
-
-'test python'
-
-__author__='lx'
-
-print("hello world")
