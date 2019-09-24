bb49375d8ec2ce21ec2f73c570cd0068835d2fee
liuxu <liuxu@xiaozhu.com>
2019-09-24 16:22:29 +0800

    up

---
+++ python/Infrastructure_data_structure/list.py
@@ -0,0 +1,35 @@
+#!
+# -*- coding= utf-8 -*-
+
+''
+
+__author__ = ""
+
+# 创建一个列表，只要把逗号分隔的不同的数据项使用方括号括起来即可。
+
+# 列表脚本操作符
+# +
+# *
+# in
+# for x in list
+
+# 列表截取与拼接
+
+# 嵌套列表
+
+# 列表函数&方法
+# len
+# max
+# min
+# list
+# append
+# count
+# extend
+# index
+# insert
+# pop
+# remove
+# reverse
+# sort
+# clear
+# copy
