4bd0d8708c3874965800d9c1cd373eeb26c056f6
Xu Liu <xu.liu@shopee.com>
2021-06-11 19:47:14 +0800

    up

---
+++ python/infrastructure_data_structure/multiprocessing/process1.py
@@ -0,0 +1,12 @@
+#! /user/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+if __name__ == '__main__':
+    print('Process (%s) start...' % os.getpid())
+    pid = os.fork()
+    if pid == 0:
+        print('I am child multiprocessing (%s) and my parent is %s.' % (os.getpid(), os.getppid()))
+    else:
+        print('I (%s) just created a child multiprocessing (%s)' % (os.getpid(), pid))
---
+++ python/infrastructure_data_structure/multiprocessing/process2.py
@@ -0,0 +1,22 @@
+#! /user/bin/env python
+# -*- coding: utf-8 -*-
+
+from multiprocessing import Process
+import os
+import time
+
+
+def run_process(name):
+    print('Run child multiprocessing %s (%s)...' % (name, os.getpid()))
+    time.sleep(3)
+    print('Child multiprocessing end.')
+
+
+if __name__ == '__main__':
+    print('Parent multiprocessing %s.' % os.getpid())
+    p = Process(target=run_process, args=('test',))
+    print('Child multiprocessing will start.')
+    p.start()
+    time.sleep(1)
+    p.join()
+    print('Parend multiprocessing end.')
